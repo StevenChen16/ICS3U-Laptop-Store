ad117d133a38db1b5d22c18dbd41c6f43aef255d
Steven Chen <117523987+StevenChen16@users.noreply.github.com>
2024-11-26 20:07:45 -0500

    Add method to launch the title frame and update main method to call it
    * **launchTitleFrame method**
      - Create a new method to launch the title frame
      - Instantiate `LaptopStoreTitleFrame` in the new method
    
    * **main method**
      - Replace the call to `LaptopStoreFileInput.fillLaptops()` with a call to `launchTitleFrame()`

--- LaptopStoreApplication.java
+++ LaptopStoreApplication.java
@@ -1,16 +1,13 @@
-
-//laptop store application class
-
 public class LaptopStoreApplication {
 	
-	//laptop array has 30 items, for each of the laptops
 	public static Laptop[] laptopArray = new Laptop[30];
 	
-	//main method calls the other methods
 	public static void main(String[]args) {
-		
 		LaptopStoreFileInput.fillLaptops();
-		
+		launchTitleFrame();
 	}
 	
+	public static void launchTitleFrame() {
+		new LaptopStoreTitleFrame();
+	}
 }
